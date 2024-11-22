35f6b9f124041f0a9cbf2a0b6033ca43cc557d29
robseks-dev <robertocarlosberrio@gmail.com>
2024-11-22 11:32:26 -0500

    Actualizacion de datos

--- src/models/employees.model.js
+++ src/models/employees.model.js
@@ -3,7 +3,7 @@ import { connection } from "../database-config.js"
 export class EmployeesModel {
   static getEmployees = async () => {
     try {
-      const [employees] = await connection.query('SELECT * FROM employee');
+      const [employees] = await connection.query('SELECT * FROM employees');
       return employees;
     } catch (error) {
       throw new Error(error.message);
@@ -12,7 +12,7 @@ export class EmployeesModel {
 
   static getEmployeeById = async ({ id }) => {
     try {
-      const [employee] = await connection.query('SELECT * FROM employee WHERE id = ?', [id]);
+      const [employee] = await connection.query('SELECT * FROM employees WHERE id = ?', [id]);
       return employee[0];
     } catch (error) {
       throw new Error(error.message);
@@ -21,9 +21,9 @@ export class EmployeesModel {
 
   static createEmployee = async ({ data }) => {
     try {
-      const { name, salary } = data;
-      const [rows] = await connection.query('INSERT INTO employee (name, salary) VALUES (?, ?)', [name, salary]);
-      return { id: rows.insertId, name, salary };
+      const { id, name, email, salary } = data;
+      await connection.query('INSERT INTO employees (id, name, email, salary) VALUES (?, ?, ?, ?)', [id, name, email, salary]);
+      return { id, name, email, salary };
     } catch (error) {
       throw new Error(error.message);
     }
@@ -31,8 +31,8 @@ export class EmployeesModel {
 
   static updateEmployee = async ({ data, id }) => {
     try {
-      const { name, salary } = data;
-      const [result] = await connection.query('UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?', [name, salary, id]);
+      const { name, email, salary } = data;
+      const [result] = await connection.query('UPDATE employees SET name = IFNULL(?, name), email = IFNULL(?, email), salary = IFNULL(?, salary) WHERE id = ?', [name, email, salary, id]);
       return result;
     } catch (error) {
       throw new Error(error.message);
@@ -41,7 +41,7 @@ export class EmployeesModel {
 
   static deleteEmployee = async ({ id }) => {
     try {
-      const [result] = await connection.query('DELETE FROM employee WHERE id = ?', [id]);
+      const [result] = await connection.query('DELETE FROM employees WHERE id = ?', [id]);
       return result;
     } catch (error) {
       throw new Error(error.message);
